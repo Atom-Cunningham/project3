1955c97c067263e5d759f76c433ee8365a60c611
honestus-codice <laser@email.arizona.edu>
2019-11-13 12:04:01 -0700

    stubbs

---
+++ src/mymalloc.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+    /**With each request, increment brk by size
+     * return old brk
+     * 
+     */
+int *my_firstfit_malloc(int size){
+    
+     
+    
+    return 0;
+}
+
+/**take a pointer to the start of the region
+ * which should be LL bookeeping data
+ * 
+ */
+int my_free(void *ptr){
+    return 0;
+}
---
+++ src/mymalloc.h
@@ -0,0 +1,7 @@
+//prototype
+
+#ifndef MYMALLOC.H
+#define MYMALLOC.H
+void *my_firstfit_malloc(int);
+void my_free(void);
+#endif
